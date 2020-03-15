95423d753000b35ca56b3e382600e62ab369221b
Zoltan Altfatter <altfatterz@gmail.com>
2020-03-15 19:26:08 +0100

    scaling kafka streams applicaiton

--- solution/deployment/temp-producer/src/main/java/streams/TempProducer.java
+++ solution/deployment/temp-producer/src/main/java/streams/TempProducer.java
@@ -56,7 +56,7 @@ public class TempProducer {
 
     private static Properties getConfig(){
         Properties props = new Properties();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:19092");
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
         return props;
